ab57aff7032fb5ef108484a529740709556629f4
ahofmann <ahofmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-03 16:33:46 +0000

    included volume correction and salinity correction in titration changed arguments for titration (beware: TAfit is not adapted to the "new" titration function yet, i.e. it does NOT work in this commit)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aquaenv@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/AquaEnv/R/aquaenv_public_applications.R
+++ pkg/AquaEnv/R/aquaenv_public_applications.R
@@ -1,26 +1,68 @@
 # PUBLIC function:
-# creates an object of class aquaenv which contains a titration simulation (changes in [Na] and [Cl] and therefore in S are assumed negligible (~ 5 mmol/kg wrt 500 mmol/kg))
+# creates an object of class aquaenv which contains a titration simulation 
 titration <- function(aquaenv,                # an object of type aquaenv: minimal definition, contains all information about the system: T, S, d, total concentrations of nutrients etc 
-                      volume,                 # the volume of the (theoretical) titration vessel in l 
-                      amount,                 # the amount of titrant added in mol
-                      steps,                  # the amount of steps the amount of titrant is added in 
+                      mass_sample,            # the mass of the sample solution in kg
+                      mass_titrant,           # the total mass of the added titrant solution in kg
+                      conc_titrant,           # the concentration of the titrant solution in mol/kg-soln
+                      S_titrant=NULL,         # the salinity of the titrant solution, if not supplied it is assumed that the titrant solution has the same salinity as the sample solution
+                      steps,                  # the amount of steps the mass of titrant is added in 
                       type)                   # the type of titrant: either "HCl" or "NaOH"
   {
-    directionTAchange   <- switch(type, HCl  = -1, NaOH = +1)
-    TAconcchangeperstep <- convert(((amount/steps)/volume), "conc", "molar2molin", aquaenv$Tc, aquaenv$S)
+    concstep <- function(conc, oldmass, newmass)
+      {
+        return((conc*oldmass)/(newmass))
+      }
 
-    aquaenvtemp <- aquaenv
+    if (is.null(S_titrant))
+      {
+        S_titrant <- aquaenv$S
+      }
+    
+    directionTAchange   <- switch(type, HCl  = -1, NaOH = +1)
+    TAmolchangeperstep  <- (conc_titrant*mass_titrant)/steps
+    masschangeperstep   <- mass_titrant/steps
+
+    aquaenv[["mass"]]   <- mass_sample
     
     for (i in 1:steps)
       {
-        TA          <- aquaenvtemp$TA + (directionTAchange * TAconcchangeperstep)
-        aquaenvtemp <- cloneaquaenv(aquaenvtemp, TA=TA)
-        aquaenv     <- merge(aquaenv, aquaenvtemp)
-      }
+        with(aquaenv,
+             {
+               i           <- length(mass)
+               
+               newmass     <- mass[[i]] + masschangeperstep
+               
+               SumCO2      <- concstep(SumCO2[[i]],   mass[[i]], newmass)
+               SumNH4      <- concstep(SumNH4[[i]],   mass[[i]], newmass)
+               SumH2S      <- concstep(SumH2S[[i]],   mass[[i]], newmass)
+               SumH3PO4    <- concstep(SumH3PO4[[i]], mass[[i]], newmass)
+               SumSiOH4    <- concstep(SumSiOH4[[i]], mass[[i]], newmass)
+               SumHNO3     <- concstep(SumHNO3[[i]],  mass[[i]], newmass)
+               SumHNO2     <- concstep(SumHNO2[[i]],  mass[[i]], newmass)
+               SumBOH3     <- concstep(SumBOH3[[i]],  mass[[i]], newmass)
+               SumH2SO4    <- concstep(SumH2SO4[[i]], mass[[i]], newmass)
+               SumHF       <- concstep(SumHF[[i]],    mass[[i]], newmass)
+               
+               S           <- (S[[i]]*mass[[i]] + S_titrant*masschangeperstep)/newmass
+               
+               TA          <- (TA[[i]]*mass[[i]] + (directionTAchange * TAmolchangeperstep))/newmass
 
-    aquaenv[["DeltaCTitrant"]] <- convert((amount/volume)/steps*(1:(steps+1)), "conc", "molar2molin", aquaenv$Tc, aquaenv$S)
+               aquaenvtemp <- aquaenv(Tc=Tc[[i]], S=S, d=d[[i]], SumCO2=SumCO2, SumNH4=SumNH4, SumH2S=SumH2S, SumH3PO4=SumH3PO4, SumSiOH4=SumSiOH4, SumHNO3=SumHNO3, SumHNO2=SumHNO2,
+                                      SumBOH3=SumBOH3, SumH2SO4=SumH2SO4, SumHF=SumHF, TA=TA,
+                                      speciation=(!is.null(aquaenv$HCO3)), skeleton=(is.null(aquaenv$Na)), revelle=(!is.null(aquaenv$revelle)), dsa=(!is.null(aquaenv$dTAdH)))
+
+               aquaenvtemp[["mass"]] <- newmass
+               aquaenv               <<- merge(aquaenv, aquaenvtemp)              
+             })
+        }
+
+    aquaenv[["delta_conc_titrant"]]   <- ((0:steps)*TAmolchangeperstep)/aquaenv[["mass"]] ; attr(aquaenv[["delta_conc_titrant"]], "unit")  <- "mol/kg-soln"
+    aquaenv[["delta_mass_titrant"]]   <- (0:steps)*masschangeperstep                      ; attr(aquaenv[["delta_mass_titrant"]], "unit")  <- "kg"
+    aquaenv[["delta_moles_titrant"]]  <- (0:steps)*TAmolchangeperstep                     ; attr(aquaenv[["delta_moles_titrant"]], "unit") <- "mol"
+
     return(aquaenv)                           # object of class aquaenv which contains a titration simulation
   }
+
 
 
 # PUBLIC function:
